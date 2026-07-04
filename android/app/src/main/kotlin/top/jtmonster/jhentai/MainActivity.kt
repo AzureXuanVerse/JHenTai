a1500fb1da8814c2d42ea5aa36d6cee0f11966e8
酱天小禽兽 <jiangtian616@qq.com>
2026-07-05 00:39:54 +0800

    feat: refactor volume event handling and integrate volume_key_board package

--- android/app/src/main/kotlin/top/jtmonster/jhentai/MainActivity.kt
+++ android/app/src/main/kotlin/top/jtmonster/jhentai/MainActivity.kt
@@ -14,28 +14,9 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterFragmentActivity() {
-    private var interceptVolumeEvent = false
-    private lateinit var volumeMethodChannel: MethodChannel
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-
-        volumeMethodChannel = MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            "top.jtmonster.jhentai.volume.event.intercept"
-        )
-
-        volumeMethodChannel.setMethodCallHandler { call, result ->
-            if (call.method == "set") {
-                val value = call.arguments<Boolean>()
-                if (value != null) {
-                    interceptVolumeEvent = value
-                }
-                result.success(null)
-            } else {
-                result.notImplemented()
-            }
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,28 +29,5 @@ class MainActivity : FlutterFragmentActivity() {
         }
 
         super.onCreate(savedInstanceState)
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if (interceptVolumeEvent && (keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
-            volumeMethodChannel.invokeMethod(
-                "event",
-                if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) 1 else -1
-            )
-            return true
-        }
-        return super.onKeyDown(keyCode, event)
-    }
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        if (interceptVolumeEvent && (keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
-            return true
-        }
-        return super.onKeyUp(keyCode, event)
-    }
-
-    override fun onDestroy() {
-        volumeMethodChannel.setMethodCallHandler(null)
-        super.onDestroy()
     }
 }
--- lib/src/service/volume_service.dart
+++ lib/src/service/volume_service.dart
@@ -1,18 +1,13 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:volume_key_board/volume_key_board.dart';
 
 import 'jh_service.dart';
-import 'log.dart';
 
 VolumeService volumeService = VolumeService();
 
 class VolumeService extends GetxService with JHLifeCircleBeanErrorCatch implements JHLifeCircleBean {
-  late final MethodChannel methodChannel;
-
-  static const int volumeUp = 1;
-  static const int volumeDown = -1;
+  Function(VolumeEventType)? _onData;
+  bool _isIntercepting = false;
 
   @override
   Future<void> doInitBean() async {
@@ -20,57 +15,42 @@ class VolumeService extends GetxService with JHLifeCircleBeanErrorCatch implemen
   }
 
   @override
-  Future<void> doAfterBeanReady() async {
-    if (!GetPlatform.isAndroid) {
-      return;
-    }
-    methodChannel = const MethodChannel('top.jtmonster.jhentai.volume.event.intercept');
-  }
+  Future<void> doAfterBeanReady() async {}
 
   @override
   void onClose() {
     super.onClose();
-
     cancelListen();
   }
 
   Future<void> setInterceptVolumeEvent(bool value) async {
-    if (!GetPlatform.isAndroid) {
-      return;
+    _isIntercepting = value;
+    if (value && _onData != null) {
+      VolumeKeyBoard.instance.addListener(_onVolumeKeyEvent);
+    } else {
+      VolumeKeyBoard.instance.removeListener();
     }
+  }
 
-    try {
-      await methodChannel.invokeMethod('set', value);
-    } on PlatformException catch (e) {
-      log.error('Set intercept volume event error!', e);
-      log.uploadError(e);
+  void _onVolumeKeyEvent(VolumeKey event) {
+    if (event == VolumeKey.up) {
+      _onData?.call(VolumeEventType.volumeUp);
+    } else if (event == VolumeKey.down) {
+      _onData?.call(VolumeEventType.volumeDown);
     }
   }
 
   void listen(Function(VolumeEventType)? onData) {
-    if (!GetPlatform.isAndroid) {
-      return;
+    _onData = onData;
+    if (_isIntercepting) {
+      VolumeKeyBoard.instance.addListener(_onVolumeKeyEvent);
     }
-
-    methodChannel.setMethodCallHandler((MethodCall call) {
-      if (call.method == 'event') {
-        final int eventType = call.arguments as int;
-        if (eventType == volumeUp) {
-          onData?.call(VolumeEventType.volumeUp);
-        } else if (eventType == volumeDown) {
-          onData?.call(VolumeEventType.volumeDown);
-        }
-      }
-      return Future.value();
-    });
   }
 
   void cancelListen() {
-    if (!GetPlatform.isAndroid) {
-      return;
-    }
-
-    methodChannel.setMethodCallHandler(null);
+    VolumeKeyBoard.instance.removeListener();
+    _onData = null;
+    _isIntercepting = false;
   }
 }
 
